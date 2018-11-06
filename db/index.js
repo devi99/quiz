93cc43cb6cf402b987f549790482acdab3d246cb
Lennert Dael <lennert.dael@live.com>
2018-11-07 00:39:07 +0100

    heroku

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL || "postgres://localhost:5432/quiz"
 });
 
 pool.on('connect', () => {
